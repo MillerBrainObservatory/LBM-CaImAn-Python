f546d6444c83e6c71e0006107b1bb542eb2be5a4
FlynnOConnell <FlynnOConnell@gmail.com>
2024-02-04 16:00:28 -0500

    begin parameter description in docstring

--- lbm_util/params.py
+++ lbm_util/params.py
@@ -5,6 +5,25 @@ def init_params():
     """
     Return a dictionary with parameters.
 
+    Parameters:
+    ----------
+    debug: bool : Whether to return debug messages.
+    chans_order_{1, 15, 30} : Channel / plane reordering to put in order of tissue depth.
+    save_output : If true, will save each plane in a separate folder, otherwise saves the full volume
+    raw_data_dirs : Absolute path to the folder containing your data. Must be a list of 1 or more directories.
+    fname_must_contain : Optional string to match filenames to exclude from the analysis.
+    fname_must_NOT_contain: Optional string to match filenames to exclude from the analysis.
+    make_template_seams_and_plane_alignment : True,
+    econstruct_all_files : True,
+    list_files_for_template : TBD.
+    json_logging : False,  # replaced with debug param
+    seams_overlap : "calculate",
+    make_nonan_volume : True,
+    lateral_align_planes : False,
+    add_1000_for_nonegative_volume : True,
+    save_mp4 : True,
+    save_meanf_png : True,
+
     TODO: Add checks with clear warnings or errors for each parameter
     TODO: detect the number of planes based on the file metadata and not on the filename
     TODO: detect the number of planes based on the file metadata and not on the filename
