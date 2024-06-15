8873a772ddc63fba9486ed1fde032f4a5cb7a1ca
Flynn OConnell <flynnoconnell@gmail.com>
2024-06-14 20:00:38 -0400

    docs: add notebooks with nbsphinx

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,15 @@ source_suffix = {
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'exclude']
 
-extensions = ["sphinx.ext.autodoc","sphinxcontrib.images", "sphinxcontrib.video", "numpydoc", "sphinx.ext.intersphinx", "sphinx.ext.napoleon", "sphinx.ext.autosectionlabel"]
+extensions = ["sphinx.ext.autodoc",
+              "nbsphinx",
+              "sphinxcontrib.images",
+              "sphinxcontrib.video",
+              "numpydoc",
+              "sphinx.ext.intersphinx",
+              "sphinx.ext.napoleon",
+              "sphinx.ext.autosectionlabel"
+              ]
 
 images_config = dict(backend='LightBox2',
                      default_image_width='100%',
@@ -33,6 +41,10 @@ html_static_path = ["_static"]
 
 html_css_files = ['LBM_docs.css']
 html_logo = "_static/lbm_icon.ico"
+
+nbsphinx_custom_formats = {
+    ".md": ["jupytext.reads", {"fmt": "mystnb"}],
+}
 
 html_theme_options = {
   "external_links": [
