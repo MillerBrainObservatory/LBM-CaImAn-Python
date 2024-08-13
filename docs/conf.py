6f31c0a66dfbf57782a86caf466ff4a67df7ee48
Flynn OConnell <flynnoconnell@gmail.com>
2024-08-13 17:46:11 -0400

    docs: images, js, markdown fix

--- docs/conf.py
+++ docs/conf.py
@@ -6,45 +6,52 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
+from pathlib import Path
 
 os.path.abspath(os.path.join(".."))
 os.path.abspath(os.path.join("..", "demos"))
 os.path.abspath(os.path.join("..", "demos", "notebooks"))
 
-project = 'LBM-CaImAn-Python'
-copyright = '2024, Flynn OConnell'
-author = 'Flynn OConnell'
-release = '0.1.0'
+project = "LBM-CaImAn-Python"
+copyright = "2024, Elizabeth R. Miller Brain Observatory | The Rockefeller University. All Rights Reserved"
+author = "Flynn OConnell"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-exclude_patterns = ['Thumbs.db', '.DS_Store']
+exclude_patterns = ["Thumbs.db", ".DS_Store"]
+myst_enable_extensions = [
+    "colon_fence",
+    "dollarmath",
+    "html_image",
+]
 
 extensions = [
     "sphinx.ext.autodoc",
     "sphinxcontrib.images",
-    "sphinxcontrib.video" ,
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.autosectionlabel",
-    "numpydoc",
+    "sphinxcontrib.video",
+    "sphinxcontrib.matlab",
+    # "myst_parser",
     "myst_nb",
-    'sphinx.ext.mathjax',
-    'sphinx_design',
+    "sphinx_copybutton",
+    "numpydoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx_togglebutton",
+    "sphinx_design",
+    "sphinx_tippy",
 ]
 
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.ipynb': 'myst-nb',
-    '.md': 'myst-nb',
+    ".rst": "restructuredtext",
+    ".ipynb": "myst-nb",
+    ".md": "myst-nb",
 }
 
-
 templates_path = ["_templates"]
-html_static_path = ['_static']
-# html_use_modindex = False
-# html_use_index = False
+html_static_path = ["_static"]
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_title = "LBM-CaImAn-Python"
@@ -53,29 +60,74 @@ html_title = "LBM-CaImAn-Python"
 # of the sidebar.
 html_logo = "_static/caiman-python-logo.svg"
 
-# The name of an image file (relative to this directory) to use as a favicon of
-# the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
-html_favicon = "_static/lbm.ico"
+html_favicon = "./_static/icon_caiman_python.png"
 
-html_theme = 'sphinx_book_theme'
+html_theme = "sphinx_book_theme"
 html_title = "LBM-CaImAn-Python"
 html_short_title = "MBO.py"
-html_static_path = ['_static']
+html_static_path = ["_static"]
 html_css_files = ["custom.css"]
 html_use_modindex = True
 html_copy_source = False
-html_file_suffix = '.html'
+html_file_suffix = ".html"
 
+current_filepath = Path()
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3.5', None),
-    'numpydoc': ('https://numpydoc.readthedocs.io/en/latest', None),
+    "python": ("https://docs.python.org/3.5", None),
+    "numpydoc": ("https://numpydoc.readthedocs.io/en/latest", None),
+    "mbo": (
+        "https://millerbrainobservatory.github.io/",
+        None,
+    ),
 }
+
+templates_path = ["_templates"]
+
+html_theme = "sphinx_book_theme"
+
+html_logo = "_static/CaImAn-MATLAB_logo.svg"
+html_short_title = "LBM CaImAn Pipeline"
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
+# html_js_files = ["subtoc.js"]
+html_favicon = "./_static/lbm_caiman_mat.svg"
+html_copy_source = True
 
 intersphinx_disabled_reftypes = ["*"]
 
-# html_sidebars = {
-#     "index": [],
-#     "guide/**": ["search-field.html", "sidebar-nav-bs.html"],
-# }
-
+html_theme_options = {
+    "path_to_docs": "docs",
+    "repository_url": "https://github.com/executablebooks/sphinx-book-theme",
+    "repository_branch": "master",
+    "launch_buttons": {
+        "binderhub_url": "https://mybinder.org",
+        "colab_url": "https://colab.research.google.com/",
+        "deepnote_url": "https://deepnote.com/",
+        "notebook_interface": "jupyterlab",
+    },
+    "use_edit_page_button": True,
+    "use_source_button": True,
+    "use_issues_button": True,
+    "use_download_button": True,
+    "show_toc_level": 3,
+    "icon_links": [
+        {
+            "name": "MBO User Hub",
+            "url": "https://millerbrainobservatory.github.io/",
+            "icon": "./_static/icon_mbo_home.png",
+            "type": "local",
+        },
+        {
+            "name": "MBO Github",
+            "url": "https://github.com/MillerBrainObservatory/",
+            "icon": "fa-brands fa-github",
+            "type": "fontawesome",
+        },
+        {
+            "name": "Connect with MBO",
+            "url": "https://mbo.rockefeller.edu/contact/",
+            "icon": "fa-regular fa-address-card",
+            "type": "fontawesome",
+        },
+    ],
+}
--- docs/index.md
+++ docs/index.md
@@ -1,93 +1,31 @@
-<!-- d_hide_title: true -->
-<!-- --- -->
+# LBM-CaImAn-Python Documentation 
 
-# 🔎 Overview
+Currently, inputs to this pipeline are limited to ScanImage `.tiff` files.
 
-::::{grid}
-:reverse:
-:gutter: 3 4 4 4
-:margin: 1 2 1 2
+## Pipeline Overview
 
-:::{grid-item}
-:columns: 12 4 4 4
+WIP
 
-```{image} _static/caiman-python-logo.svg
-:width: 200px
-:class: sd-m-auto
-:name: landing-page-logo
+```{thumbnail} https://github.com/MillerBrainObservatory/static-assets/blob/master/_images/ex_diagram.png
+---
+width: 800
+align: center
+---
+
 ```
 
-:::
+----------------
 
-:::{grid-item}
-:columns: 12 8 8 8
-:child-align: justify
-:class: sd-fs-5
+## Documentation Contents
 
-```{rubric} LBM-CaImAn-Python
-```
-
-
-```{code-cell} python
-a = "This is some"
-b = "Python code!"
-print(f"{a} {b}")
-```
-
-Use the MyST role and directive syntax to harness the full capability of Sphinx, such as admonitions and figures, and all existing Sphinx extensions.
-
-```{rubric} Additional resources
-```
-
-[MyST-Markdown VS Code extension](https://marketplace.visualstudio.com/items?itemName=ExecutableBookProject.myst-highlight)
-: For MyST extended syntax highlighting and authoring tools.
-
-[Convert existing ReStructuredText files to Markdown][rst-to-myst]
-: Use the [rst-to-myst] CLI or [the MySTyc interactive web interface](https://astrojuanlu.github.io/mystyc/).
-
-[MyST-NB](https://myst-nb.readthedocs.io)
-: A Sphinx and Docutils extension for compiling Jupyter Notebooks into high quality documentation formats, built on top of the MyST-Parser.
-
-[Jupyter Book](https://jupyterbook.org)
-: An open source project for building beautiful, publication-quality books and documents from computational material, built on top of the MyST-Parser and MyST-NB.
-
-[The Jupyter Book gallery](https://executablebooks.org/en/latest/gallery)
-: Examples of documents built with MyST.
-
-[Javascript MyST parser][mystjs]
-: The [mystjs] Javascript parser, allows you to parse MyST in websites.
-
-[markdown-it-py]
-: A CommonMark-compliant and extensible Markdown parser, used by MyST-Parser to parse source text to tokens.
-
-```{rubric} Acknowledgements
-```
-
-The MyST markdown language and MyST parser are both supported by the open community,
-[The Executable Book Project](https://executablebooks.org).
 
 ```{toctree}
-:hidden:
-snippets.md
+---
+maxdepth: 2
+---
+
 api/index.md
+snippets.md
+image_gallery
+glossary
 ```
-
-[commonmark]: https://commonmark.org/
-[github-ci]: https://github.com/executablebooks/MyST-Parser/workflows/continuous-integration/badge.svg?branch=master
-[github-link]: https://github.com/executablebooks/MyST-Parser
-[codecov-badge]: https://codecov.io/gh/executablebooks/MyST-Parser/branch/master/graph/badge.svg
-[codecov-link]: https://codecov.io/gh/executablebooks/MyST-Parser
-[rtd-badge]: https://readthedocs.org/projects/myst-parser/badge/?version=latest
-[rtd-link]: https://myst-parser.readthedocs.io/en/latest/?badge=latest
-[black-badge]: https://img.shields.io/badge/code%20style-black-000000.svg
-[pypi-badge]: https://img.shields.io/pypi/v/myst-parser.svg
-[pypi-link]: https://pypi.org/project/myst-parser
-[conda-badge]: https://anaconda.org/conda-forge/myst-parser/badges/version.svg
-[conda-link]: https://anaconda.org/conda-forge/myst-parser
-[black-link]: https://github.com/ambv/black
-[github-badge]: https://img.shields.io/github/stars/executablebooks/myst-parser?label=github
-[markdown-it-py]: https://markdown-it-py.readthedocs.io/
-[markdown-it]: https://markdown-it.github.io/
-[rst-to-myst]: https://rst-to-myst.readthedocs.io
-[mystjs]: https://github.com/executablebooks/mystjs
-
