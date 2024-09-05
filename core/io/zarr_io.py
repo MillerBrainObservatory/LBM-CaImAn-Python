6d5a49c1dcdf9b6fe3db919c907cea58848aaa83
Flynn OConnell <FlynnOConnell@gmail.com>
2024-09-05 11:48:27 -0400

    feat: lazy zarr saving from scanreader

--- core/io/zarr_io.py
+++ core/io/zarr_io.py
@@ -1,9 +1,12 @@
 import os
+import time
 from pathlib import Path
 import dask.array as da
 import logging
 
+import zarr
 from scanreader import scans
+from zarr.errors import ContainsArrayError
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -26,6 +29,48 @@ def get_zarr_files(directory):
     return [x for x in directory.glob("*") if x.suffix == ".zarr"]
 
 
+def iter_planes(scan, frames, planes, xslice=slice(None), yslice=slice(None)):
+    for plane in planes:
+        yield da.squeeze(scan[frames, plane, yslice, xslice])
+
+
+def save_as_zarr(scan: scans.ScanLBM,
+                 savedir: os.PathLike,
+                 frames=slice(None),
+                 planes=slice(None),
+                 metadata=None,
+                 prepend_str='extracted',
+                 overwrite=False
+                 ):
+    filestore = zarr.DirectoryStore(savedir)
+    root = zarr.group(filestore, overwrite=overwrite)
+
+    if isinstance(frames, int):
+        frames = [frames]
+    if isinstance(planes, int):
+        planes = [planes]
+
+    iterator = iter_planes(planes)
+    logging.info(f"Selected planes: {planes}")
+    outer = time.time()
+
+    for idx, array in enumerate(iterator):
+        start = time.time()
+        try:
+            da.to_zarr(
+                arr=array,
+                url=savedir,
+                root=root,
+                component=f"plane_{idx+1}",
+                overwrite=overwrite,
+            )
+        except ContainsArrayError:
+            logging.info(f"Plane {idx+1} already exists. Skipping...")
+            continue
+        # root['preprocessed'][f'plane_{idx+1}'].attrs['fps'] = self.metadata['fps']
+        logging.info(f"Plane saved in {time.time() - start} seconds...")
+    logging.info(f"All z-planes saved in {time.time() - outer} seconds...")
+
 def save_as_zarr(scan: scans.ScanLBM,
                  savedir: os.PathLike,
                  frames=slice(None),
@@ -41,5 +86,4 @@ def save_as_zarr(scan: scans.ScanLBM,
 
     for idx in planes:
         filename = savedir / f'{prepend_str}_plane_{idx}.zarr'
-
-    da.to_zarr(data, filename)
+        da.to_zarr(scan[frames, planes, :, :], filename)
