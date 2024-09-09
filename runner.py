b6b6cd91a7b84bccc2b815a3aec7bf365468c036
Flynn OConnell <FlynnOConnell@gmail.com>
2024-09-09 17:28:37 -0400

    chore: runner without fastplotlib

--- runner.py
+++ runner.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import logging
 import mesmerize_core as mc
 from mesmerize_core.caiman_extensions.cnmf import cnmf_cache
@@ -9,24 +12,27 @@ from pathlib import Path
 import os
 import zarr
 import pandas as pd
-import napari
+
 pd.options.display.max_colwidth = 120
 
 try:
     import cv2
+
     cv2.setNumThreads(0)
 except():
     pass
 
 logging.basicConfig()
 
-os.environ["CONDA_PREFIX_1"] = ""
+os.environ['CONDA_EXE'] = str(Path().home() / 'miniconda3' / 'bin' / 'conda')
+
 if os.name == "nt":
     # disable the cache on windows, this will be automatic in a future version
     cnmf_cache.set_maxsize(0)
 
 raw_data_path = Path().home() / "caiman_data_org"
 movie_path = raw_data_path / 'animal_01' / "session_01" / 'plane_1'
+
 # moviepath
 raw_movie = zarr.open(movie_path).info
 raw_movie
@@ -40,5 +46,22 @@ try:
 except (IsADirectoryError, FileNotFoundError):
     df = mc.create_batch(batch_path)
 
-df=df.caiman.reload_from_disk()
-df
+df = df.caiman.reload_from_disk()
+
+# set up logging
+debug = True
+
+logger = logging.getLogger("caiman")
+logger.setLevel(logging.WARNING)
+handler = logging.StreamHandler()
+log_format = logging.Formatter(
+    "%(relativeCreated)12d [%(filename)s:%(funcName)10s():%(lineno)s] [%(process)d] %(message)s")
+handler.setFormatter(log_format)
+logger.addHandler(handler)
+
+if debug:
+    logging.getLogger("caiman").setLevel(logging.INFO)
+
+df.iloc[0].caiman.run()
+print(df.iloc[0].outputs['traceback'])
+x = 6
