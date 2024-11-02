d3b32954c7a708bcc793ecc407de97f43d445080
Flynn OConnell <FlynnOConnell@gmail.com>
2024-11-01 23:10:01 -0400

    feat: scaffold argument parser

--- lbm_caiman_python/__main__.py
+++ lbm_caiman_python/__main__.py
@@ -4,6 +4,9 @@ import argparse
 import numpy as np
 from lbm_caiman_python import default_ops
 
+with open("VERSION", "r") as VERSION:
+    version = VERSION.read().strip()
+
 
 def add_args(parser: argparse.ArgumentParser):
     """
@@ -23,3 +26,59 @@ def add_args(parser: argparse.ArgumentParser):
             v["type"] = type(v["default"][0])
         parser.add_argument("--" + k, **v)
     return parser
+
+
+def parse_args(parser: argparse.ArgumentParser):
+    """
+    Parses arguments and returns ops with parameters filled in.
+    From: https://github.com/MouseLand/suite2p/blob/main/suite2p/__main__.py
+    """
+    args = parser.parse_args()
+    dargs = vars(args)
+    ops0 = default_ops()
+    ops = np.load(args.ops, allow_pickle=True).item() if args.ops else {}
+    set_param_msg = "->> Setting {0} to {1}"
+    # options defined in the cli take precedence over the ones in the ops file
+    for k in ops0:
+        default_key = ops0[k]
+        args_key = dargs[k]
+        if k in ["fast_disk", "save_folder", "save_path0"]:
+            if args_key:
+                ops[k] = args_key
+                print(set_param_msg.format(k, ops[k]))
+        elif type(default_key) in [np.ndarray, list]:
+            n = np.array(args_key)
+            if np.any(n != np.array(default_key)):
+                ops[k] = n.astype(type(default_key))
+                print(set_param_msg.format(k, ops[k]))
+        elif isinstance(default_key, bool):
+            args_key = bool(int(args_key))  # bool("0") is true, must convert to int
+            if default_key != args_key:
+                ops[k] = args_key
+                print(set_param_msg.format(k, ops[k]))
+        # checks default param to args param by converting args to same type
+        elif not (default_key == type(default_key)(args_key)):
+            ops[k] = type(default_key)(args_key)
+            print(set_param_msg.format(k, ops[k]))
+    return args, ops
+
+
+def main():
+    args, ops = parse_args(
+        add_args(argparse.ArgumentParser(description="LBM-Caiman pipeline parameters")))
+    if args.version:
+        print("lbm_caiman_python v{}".format(version))
+    elif args.single_plane and args.ops:
+        from lbm_caiman_python.run_lcp import run_plane
+        # run single plane (does registration)
+        run_plane(ops, ops_path=args.ops)
+    elif len(args.db) > 0:
+        db = np.load(args.db, allow_pickle=True).item()
+        from lbm_caiman_python import run_lcp
+        run_lcp(ops)
+    else:
+        print('else')
+
+
+if __name__ == "__main__":
+    main()
