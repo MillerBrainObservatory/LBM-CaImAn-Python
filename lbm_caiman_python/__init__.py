7d2106d4e6d3081dd3ba86924abc34c5c6913076
Flynn OConnell <FlynnOConnell@gmail.com>
2024-11-18 14:06:18 -0500

    chore: __all__ for imports

--- lbm_caiman_python/__init__.py
+++ lbm_caiman_python/__init__.py
@@ -2,5 +2,16 @@ from . import stdout
 from .default_ops import default_ops
 from .collation import combine_z_planes
 from .assembly import read_scan, get_reader
-from .io.batch import delete_batch_rows, get_batch_from_path, validate_path
+from .io.batch import delete_batch_rows, get_batch_from_path, validate_path, clean_batch
 
+__all__ = [
+    'stdout',
+    'default_ops',
+    'combine_z_planes',
+    'read_scan',
+    'get_reader',
+    'delete_batch_rows',
+    'get_batch_from_path',
+    'validate_path',
+    'clean_batch',
+]
