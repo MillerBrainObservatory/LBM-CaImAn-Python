de7f8d2f1ec6c8a2e44761a16b07d684d3c35815
Flynn OConnell <FlynnOConnell@gmail.com>
2024-11-20 14:36:46 -0500

    chore: clean import order (remove io package)

--- lbm_caiman_python/__init__.py
+++ lbm_caiman_python/__init__.py
@@ -1,8 +1,8 @@
 from . import stdout
 from .default_ops import default_ops
 from .collation import combine_z_planes
-from .assembly import read_scan, get_reader
-from .io.batch import delete_batch_rows, get_batch_from_path, validate_path, clean_batch
+from .assembly import read_scan, get_reader, fix_scan_phase, return_scan_offset
+from .batch import delete_batch_rows, get_batch_from_path, validate_path, clean_batch
 
 __all__ = [
     "stdout",
@@ -14,4 +14,6 @@ __all__ = [
     "get_batch_from_path",
     "validate_path",
     "clean_batch",
+    "fix_scan_phase",
+    "return_scan_offset",
 ]
--- lbm_caiman_python/io/__init__.py
+++
@@ -1,6 +0,0 @@
-from .batch import (
-    delete_batch_rows,
-    get_batch_from_path,
-    clean_batch,
-    validate_path,
-)
