52ca50856e35afb81933b899d13ea2531fbff612
Flynn OConnell <flynnoconnell@gmail.com>
2024-11-20 22:55:30 -0500

    chore: remove old reader

--- lbm_caiman_python/__init__.py
+++ lbm_caiman_python/__init__.py
@@ -1,20 +1,21 @@
 from . import stdout
 from .default_ops import default_ops
 from .collation import combine_z_planes
-from .assembly import read_scan, get_reader, fix_scan_phase, return_scan_offset
+from .assembly import read_scan, fix_scan_phase, return_scan_offset
 from .batch import delete_batch_rows, get_batch_from_path, validate_path, clean_batch
-from .util.io import get_metadata
+from .util.io import get_metadata, get_files
 
 __all__ = [
     "stdout",
     "default_ops",
     "combine_z_planes",
     "read_scan",
-    "get_reader",
     "delete_batch_rows",
     "get_batch_from_path",
     "validate_path",
     "clean_batch",
     "fix_scan_phase",
     "return_scan_offset",
+    "get_files",
+    "get_metadata",
 ]
