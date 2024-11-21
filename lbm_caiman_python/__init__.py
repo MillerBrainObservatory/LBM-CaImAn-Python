3f1d07712e0210c3c3eaca5678a25dc72fc28fa3
Flynn OConnell <FlynnOConnell@gmail.com>
2024-11-21 13:56:08 -0500

    chore: add save_as to package imports

--- lbm_caiman_python/__init__.py
+++ lbm_caiman_python/__init__.py
@@ -1,7 +1,7 @@
 from . import stdout
 from .default_ops import default_ops
 from .collation import combine_z_planes
-from .assembly import read_scan, fix_scan_phase, return_scan_offset
+from .assembly import read_scan, fix_scan_phase, return_scan_offset, save_as
 from .batch import delete_batch_rows, get_batch_from_path, validate_path, clean_batch
 from .util.io import get_metadata, get_files
 
@@ -18,4 +18,5 @@ __all__ = [
     "return_scan_offset",
     "get_files",
     "get_metadata",
+    "save_as",
 ]
