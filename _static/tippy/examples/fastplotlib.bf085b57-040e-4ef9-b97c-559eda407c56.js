86bd95a361ee066a3405a67014650578ba7d9d2f
FlynnOConnell <FlynnOConnell@users.noreply.github.com>
2024-11-25 00:02:33 +0000

    deploy: a2ac7a5e07e2889998431fcec7adfed8e4d59032

---
+++ _static/tippy/examples/fastplotlib.bf085b57-040e-4ef9-b97c-559eda407c56.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"#preview-raw-traces\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">25.2. </span>Preview \u2018raw traces\u2019<a class=\"headerlink\" href=\"#preview-raw-traces\" title=\"Permalink to this heading\">#</a></h2><p>Show the raw trace for a given pixel by clicking on that pixel.</p>", "a[href=\"#fastplotlib-examples\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">25. </span>Fastplotlib Examples<a class=\"headerlink\" href=\"#fastplotlib-examples\" title=\"Permalink to this heading\">#</a></h1><h2><span class=\"section-number\">25.1. </span>Smoothing with Gaussian Filter<a class=\"headerlink\" href=\"#smoothing-with-gaussian-filter\" title=\"Permalink to this heading\">#</a></h2>", "a[href=\"#smoothing-with-gaussian-filter\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\"><span class=\"section-number\">25.1. </span>Smoothing with Gaussian Filter<a class=\"headerlink\" href=\"#smoothing-with-gaussian-filter\" title=\"Permalink to this heading\">#</a></h2>"}
+skip_classes = ["headerlink", "sd-stretched-link"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(` ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
