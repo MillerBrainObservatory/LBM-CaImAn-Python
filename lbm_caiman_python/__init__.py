0646f441fe1c4c76feb44ba262be7cad6255d576
Flynn OConnell <flynnoconnell@gmail.com>
2024-12-01 18:02:20 -0500

    new helpers as imports

--- lbm_caiman_python/__init__.py
+++ lbm_caiman_python/__init__.py
@@ -1,9 +1,13 @@
+from . import _version
 from . import stdout
 from .default_ops import default_ops
 from .collation import combine_z_planes
 from .assembly import read_scan, fix_scan_phase, return_scan_offset, save_as
 from .batch import delete_batch_rows, get_batch_from_path, validate_path, clean_batch
 from .util.io import get_metadata, get_files
+from .helpers import generate_patch_view, plot_with_scalebars
+
+__version__ = _version.get_versions()['version']
 
 __all__ = [
     "stdout",
@@ -19,7 +23,6 @@ __all__ = [
     "get_files",
     "get_metadata",
     "save_as",
+    "generate_patch_view",
+    "plot_with_scalebars",
 ]
-
-from . import _version
-__version__ = _version.get_versions()['version']
