eb892db6e752b6212785609839ba980494b9f5a7
Flynn <Flynnoconnell@gmail.com>
2025-01-14 14:48:53 -0500

    refact: remove buggy get_files

--- lbm_caiman_python/lcp_io.py
+++ lbm_caiman_python/lcp_io.py
@@ -114,58 +114,6 @@ def get_metadata(file: os.PathLike | str):
         raise ValueError(f"No metadata found in {file}.")
 
 
-def get_files(
-        pathnames: os.PathLike | str | list[os.PathLike | str],
-        ext: str = 'tif',
-        exclude_pattern: str = '_plane_',
-) -> list[os.PathLike | str] | os.PathLike:
-    """
-    Expands a list of pathname patterns to form a sorted list of absolute filenames.
-
-    Parameters
-    ----------
-    pathnames: os.PathLike
-        Pathname(s) or pathname pattern(s) to read.
-    ext: str
-        Extention, string giving the filetype extention.
-    exclude_pattern: str | list
-        A string or list of strings that match to files marked as excluded from processing.
-
-    Returns
-    -------
-    List[PathLike[AnyStr]]
-        List of absolute filenames.
-    """
-    if '.' in ext or 'tiff' in ext:
-        ext = 'tif'  #glob tiff and tif
-    if isinstance(pathnames, (list, tuple)):
-        out_files = []
-        excl_files = []
-        for fpath in pathnames:
-            if exclude_pattern not in str(fpath):
-                if Path(fpath).is_file():
-                    out_files.extend([fpath])
-                elif Path(fpath).is_dir():
-                    fnames = [x for x in Path(fpath).expanduser().glob(f"*{ext}*")]
-                    out_files.extend(fnames)
-            else:
-                excl_files.extend(fpath)
-        return sorted(out_files)
-    if isinstance(pathnames, (os.PathLike, str)):
-        pathnames = Path(pathnames).expanduser()
-        if pathnames.is_dir():
-            files_with_ext = [x for x in pathnames.glob(f"*{ext}*")]
-            return sorted(files_with_ext)
-        elif pathnames.is_file():
-            if exclude_pattern not in str(pathnames):
-                return pathnames
-            else:
-                raise FileNotFoundError(f"No {ext} files found in directory: {pathnames}")
-    else:
-        raise ValueError(
-            f"Input path should be an iterable list/tuple or PathLike object (string, pathlib.Path), not {pathnames}")
-
-
 def get_files_ext(base_dir, extension, max_depth) -> list:
     """
     Recursively searches for files with a specific extension up to a given depth and stores their paths in a pickle file.
