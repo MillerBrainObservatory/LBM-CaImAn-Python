7c124e8d5f5c2b30f4fb320d814153fe5441ff8c
Flynn <Flynnoconnell@gmail.com>
2025-01-18 17:19:54 -0500

    expose run_gui with QApplication and LBMMainWindow

---
+++ lbm_caiman_python/gui/rungui.py
@@ -0,0 +1,13 @@
+import sys
+
+from qtpy.QtWidgets import QApplication
+from lbm_caiman_python.gui.widgets import LBMMainWindow
+
+
+def run_gui():
+    app = QApplication(sys.argv)
+    main_window = LBMMainWindow()
+    print('--')
+    main_window.show()
+    app.exec()
+    # fpl.loop.run()
