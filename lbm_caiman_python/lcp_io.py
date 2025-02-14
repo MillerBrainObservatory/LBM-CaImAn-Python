25a58c0592a4b2cc6eb8f0248242797acbe0ecba
Flynn <Flynnoconnell@gmail.com>
2025-02-14 15:33:44 -0500

    import module relative, add fov_roi_um metadata

--- lbm_caiman_python/lcp_io.py
+++ lbm_caiman_python/lcp_io.py
@@ -1,12 +1,19 @@
 import os
 import re
 
+import ffmpeg
+import numpy
+
 import numpy as np
 import tifffile
 from pathlib import Path
 from glob import glob
 from os import path
+
+from matplotlib import pyplot as plt, colormaps as cm
 from scanreader import scans
+
+from .util.signal import norm_minmax
 
 
 def expand_wildcard(wildcard):
@@ -211,17 +218,20 @@ def get_metadata(file: os.PathLike | str):
         # Field-of-view calculations
         # TODO: We may want an FOV measure that takes into account contiguous ROIs
         # As of now, this is for a single ROI
-        fov_x = round(objective_resolution * size_xy[0])
-        fov_y = round(objective_resolution * size_xy[1])
+        fov_x = round(objective_resolution * size_xy[0])  # in microns
+        fov_y = round(objective_resolution * size_xy[1])  # in microns
+        fov_roi_um = (fov_x, fov_y)                       # in microns
         fov_xy = (int(fov_x), int(fov_y / num_rois))
+        fov_px = (int(fov_x / num_pixel_xy[0]), int(fov_y / num_pixel_xy[1]))
 
-        # Pixel resolution (dxy) calculation
         pixel_resolution = (fov_x / num_pixel_xy[0], fov_y / num_pixel_xy[1])
 
         return {
             "num_planes": num_planes,
             "num_frames": int(len(pages) / num_planes),
             "fov": fov_xy,  # in microns
+            "fov_px": fov_px,
+            "fov_roi_um": fov_roi_um,
             "num_rois": num_rois,
             "frame_rate": frame_rate,
             "pixel_resolution": np.round(pixel_resolution, 2),
@@ -240,7 +250,7 @@ def get_metadata(file: os.PathLike | str):
         raise ValueError(f"No metadata found in {file}.")
 
 
-def get_files_ext(base_dir, str_contains, max_depth, sorted=False) -> list:
+def get_files_ext(base_dir, str_contains, max_depth=1, sorted=True) -> list | Path:
     """
     Recursively searches for files with a specific extension up to a given depth and stores their paths in a pickle file.
 
@@ -249,7 +259,7 @@ def get_files_ext(base_dir, str_contains, max_depth, sorted=False) -> list:
     base_dir : str or Path
         The base directory to start searching.
     str_contains : str
-        The file extension to look for (e.g., '.txt').
+        The string that the file names should contain.
     max_depth : int
         The maximum depth of subdirectories to search.
     sorted : bool, optional
@@ -268,7 +278,7 @@ def get_files_ext(base_dir, str_contains, max_depth, sorted=False) -> list:
 
     files = [
         file for file in base_path.rglob(f'*{str_contains}*')
-        if len(file.relative_to(base_path).parts) <= max_depth + 1
+        if len(file.relative_to(base_path).parts) <= max_depth
     ]
 
     if sorted:
@@ -325,3 +335,110 @@ def stack_from_files(files: list):
 
     zstack = np.stack(lazy_arrays, axis=1)
     return zstack
+
+
+def save_png(fname, data):
+    plt.imshow(data)
+    plt.axis('tight')
+    plt.axis('off')
+    plt.tight_layout()
+    plt.savefig(fname, dpi=300, bbox_inches='tight')
+    print(f"Saved data to {fname}")
+
+
+def save_mp4(fname: str | Path | np.ndarray, images, framerate=60, speedup=1, chunk_size=100, cmap="gray", win=7,
+             vcodec='libx264', normalize=True):
+    """
+    Save a video from a 3D array or TIFF stack to `.mp4`.
+
+    Parameters
+    ----------
+    fname : str
+        Output video file name.
+    images : numpy.ndarray or str
+        Input 3D array (T x H x W) or a file path to a TIFF stack.
+    framerate : int, optional
+        Original framerate of the video, by default 60.
+    speedup : int, optional
+        Factor to increase the playback speed, by default 1 (no speedup).
+    chunk_size : int, optional
+        Number of frames to process and write in a single chunk, by default 100.
+    cmap : str, optional
+        Colormap to apply to the video frames, by default "gray".
+        Must be a valid Matplotlib colormap name.
+    win : int, optional
+        Temporal averaging window size. If `win > 1`, frames are averaged over
+        the specified window using convolution. By default, 7.
+    vcodec : str, optional
+        Video codec to use, by default 'libx264'.
+    normalize : bool, optional
+        Flag to min-max normalize the video frames, by default True.
+
+    Raises
+    ------
+    FileNotFoundError
+        If the input file does not exist when `images` is provided as a file path.
+    ValueError
+        If `images` is not a valid 3D NumPy array or a file path to a TIFF stack.
+
+    Notes
+    -----
+    - The input array `images` must have the shape (T, H, W), where T is the number of frames,
+      H is the height, and W is the width.
+    - The `win` parameter performs temporal smoothing by averaging over adjacent frames.
+
+    Examples
+    --------
+    Save a video from a 3D NumPy array with a colormap and speedup:
+
+    >>> import numpy as np
+    >>> images = np.random.rand(100, 600, 576) * 255
+    >>> save_mp4('output.mp4', images, framerate=30, cmap='viridis', speedup=2)
+
+    Save a video with temporal averaging applied over a 5-frame window at 4x speed:
+
+    >>> save_mp4('output_smoothed.mp4', images, framerate=30, speedup=4, cmap='gray', win=5)
+
+    Save a video from a TIFF stack:
+
+    >>> save_mp4('output.mp4', 'path/to/stack.tiff', framerate=60, cmap='gray')
+    """
+    if isinstance(images, (str, Path)):
+        print(f"Loading TIFF stack from {images}")
+        if Path(images).is_file():
+            images = tifffile.memmap(images)
+        else:
+            raise FileNotFoundError(f"File not found: {images}")
+
+    T, height, width = images.shape
+    colormap = cm.get_cmap(cmap)
+
+    if normalize:
+        print("Normalizing mp4 images to [0, 1]")
+        images = norm_minmax(images)
+
+    if win and win > 1:
+        print(f"Applying temporal averaging with window size {win}")
+        kernel = np.ones(win) / win
+        images = np.apply_along_axis(lambda x: np.convolve(x, kernel, mode='same'), axis=0, arr=images)
+
+    print(f"Saving {T} frames to {fname}")
+    output_framerate = int(framerate * speedup)
+    process = (
+        ffmpeg
+        .input('pipe:', format='rawvideo', pix_fmt='rgb24', s=f'{width}x{height}', framerate=output_framerate)
+        .output(str(fname), pix_fmt='yuv420p', vcodec=vcodec, r=output_framerate)
+        .overwrite_output()
+        .run_async(pipe_stdin=True)
+    )
+
+    for start in range(0, T, chunk_size):
+        end = min(start + chunk_size, T)
+        chunk = images[start:end]
+        colored_chunk = (colormap(chunk)[:, :, :, :3] * 255).astype(np.uint8)
+        for frame in colored_chunk:
+            process.stdin.write(frame.tobytes())
+
+    process.stdin.close()
+    process.wait()
+    print(f"Video saved to {fname}")
